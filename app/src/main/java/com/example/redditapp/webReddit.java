bd1fe49c59b1e3eb9baed5505b0e0e27dacd1a12
Ever-present <s.developer.y@gmail.com>
2022-12-20 21:18:08 +0200

    Initial commit

---
+++ app/src/main/java/com/example/redditapp/webReddit.java
@@ -0,0 +1,33 @@
+package com.example.redditapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class webReddit extends AppCompatActivity {
+    private WebView webView;
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_reddit);
+
+        webView = findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.reddit.com/hot/");
+        webView.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack())
+            webView.goBack();
+        else
+            super.onBackPressed();
+    }
+}
